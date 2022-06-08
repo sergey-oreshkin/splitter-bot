7a8eef9681350063c7aff1746ac60258bb916982
sergey-oreshkin <joknife79@yandex.ru>
2022-06-08 20:31:55 +0500

    /split + name

--- src/main/java/home/serg/repisitory/SplitDao.java
+++ src/main/java/home/serg/repisitory/SplitDao.java
@@ -1,9 +1,15 @@
 package home.serg.repisitory;
 
 import home.serg.entity.Split;
+import home.serg.entity.UsersChat;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SplitDao extends CrudRepository<Split, Integer> {
+    List<Split> findAllByUsersChat(UsersChat usersChat);
+    Optional<Split> findSplitByName(String name);
 }
--- src/main/java/home/serg/service/MessageProcessorImpl.java
+++ src/main/java/home/serg/service/MessageProcessorImpl.java
@@ -1,12 +1,7 @@
 package home.serg.service;
 
-import home.serg.entity.Chat;
-import home.serg.entity.Users;
-import home.serg.entity.UsersChat;
-import home.serg.entity.UsersChatId;
-import home.serg.repisitory.ChatDao;
-import home.serg.repisitory.UsersChatDao;
-import home.serg.repisitory.UsersDao;
+import home.serg.entity.*;
+import home.serg.repisitory.*;
 import home.serg.utils.Messages;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +10,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -25,8 +21,10 @@ public class MessageProcessorImpl implements MessageProcessor {
 
     final private ChatDao chatDao;
     final private UsersDao usersDao;
-
     final private UsersChatDao usersChatDao;
+    final private SplitDao splitDao;
+
+    final private SplitRecordDao splitRecordDao;
 
     @Override
     public String getResponse(Update update) {
@@ -44,6 +42,7 @@ public class MessageProcessorImpl implements MessageProcessor {
                         .build();
 
                 User u = update.getMessage().getFrom();
+
                 Users user = Users.builder()
                         .userId(u.getId())
                         .firstName(u.getFirstName())
@@ -55,7 +54,7 @@ public class MessageProcessorImpl implements MessageProcessor {
                 usersChat.setUsers(user);
                 usersChat.setChat(chat);
 
-                if(usersChatDao.findById(usersChatId).isPresent()){
+                if (usersChatDao.findById(usersChatId).isPresent()) {
                     response = "Я тебя уже знаю, " + parseName(u);
                 } else {
                     chatDao.save(chat);
@@ -63,10 +62,8 @@ public class MessageProcessorImpl implements MessageProcessor {
                     usersDao.save(user);
                     response = "Я тебя запомнил, " + parseName(u);
                 }
-
                 break;
             case "/all":
-//
                 Chat chat1 = Chat.builder()
                         .chatId(update.getMessage().getChatId())
                         .title(update.getMessage().getChat().getTitle())
@@ -78,15 +75,65 @@ public class MessageProcessorImpl implements MessageProcessor {
                                 .map(Users::getUserId)
                                 .collect(Collectors.toList())
                 );
-                String usersString = users.stream().map(Users::getNicName).collect(Collectors.joining(","));
+                String usersString = users.stream()
+                        .map(Users::getNicName)
+                        .collect(Collectors.joining(", ")
+                        );
 
-                response = usersString +  "\nЭто все кого я знаю";
+                response = usersString + "\nЭто все кого я знаю";
                 break;
-            case "/spend":
-                response = "Я это учту";
-                break;
-            case "/calc":
-                response = "Посчитаю, но попозже";
+            case "/split":
+                chat = Chat.builder()
+                        .chatId(update.getMessage().getChatId())
+                        .title(update.getMessage().getChat().getTitle())
+                        .chatType(update.getMessage().getChat().getType())
+                        .build();
+
+                u = update.getMessage().getFrom();
+
+                user = Users.builder()
+                        .userId(u.getId())
+                        .firstName(u.getFirstName())
+                        .lastName(u.getLastName())
+                        .nicName(u.getUserName())
+                        .build();
+                usersChatId = new UsersChatId(user.getUserId(), chat.getChatId());
+
+                usersChat = new UsersChat();
+                usersChat.setUsers(user);
+                usersChat.setChat(chat);
+
+                if (parts.length == 1) { // /split (все сплиты)
+                    List<Split> splits = splitDao.findAllByUsersChat(usersChat);
+                    if (splits.isEmpty()) {
+                        response = "Ни одного сплита в этом чате";
+                    } else {
+                        response = splits.stream()
+                                .map(Split::getName)
+                                .collect(Collectors.joining(", "));
+                    }
+                } else if (parts.length == 2) {
+                    if (parts[1].startsWith("-")){ // /split -цифра
+                        //TODO удаление записи
+                    } else { // /split имя
+                        Optional<Split> splitOptional = splitDao.findSplitByName(parts[1]);
+                        if (splitOptional.isPresent()){
+                            response = "такой сплит есть"; //TODO вывод записей
+                        } else {
+                            usersChat = usersChatDao.findById(usersChatId).get();
+                            Split split = Split.builder()
+                                    .name(parts[1])
+                                    .usersChat(usersChat)
+                                    .build();
+                            splitDao.save(split);
+                            response = "Сплит " + parts[1] + " сохранен";
+                        }
+                    }
+                } else { // /split имя сумма [@nicname:доля .. @nicname:доля] (создание записи)
+
+                }
+
+
                 break;
         }
         return response;
@@ -94,5 +141,9 @@ public class MessageProcessorImpl implements MessageProcessor {
 
     private String parseName(User user) {//Todo refactor
         return user.getFirstName() + " " + user.getLastName();
+    }
+
+    static class Parts{
+
     }
 }
